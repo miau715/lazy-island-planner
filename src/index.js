a8ad9e6c19bd95460d70a46b2b08acc14b05fa49
miau715 <miau715@gmail.com>
2020-05-11 03:03:37 +0800

    basic build

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function MenuModes(props) {
 }
 
 function MenuBtn(props) {
-  const alt = props.data.mode || props.data.tool || props.data.item || props.data.colorName;
+  const id = props.data.mode || props.data.tool || props.data.item || props.data.colorName;
   let className;
   const isActive = props.isActive ? 'active' : '';
   if (props.data.colorName) {
@@ -31,11 +31,23 @@ function MenuBtn(props) {
     className = `${props.type} ${isActive}`;
   }
   if (props.data.image) {
-    return (
-      <button id={props.data.mode} onClick={props.onClick} className={className}>
-        <img alt={alt} src={props.data.image} />
-      </button>
-    )
+    if (id === 'colorCustom') {
+      return (
+        <button id={id} onClick={props.onClick} className={className}>
+          <label>
+            <input type='color' defaultValue={props.customColor} onChange={props.changeColor} />
+            <img alt={id} src={props.data.image} />
+          </label>
+        </button>
+      )
+    }
+    else {
+      return (
+        <button id={id} onClick={props.onClick} className={className}>
+          <img alt={id} src={props.data.image} />
+        </button>
+      )
+    }
   }
   else {
     const colorStyle = {color: props.data.item};
@@ -79,7 +91,7 @@ function MenuItems(props) {
   }
   const listItems = menuItems.map((tool, i) =>
     <li key={i}>
-      <MenuBtn data={tool} type={type} isActive={tool[compareTarget] === props.currentItem} onClick={props.onClick} />
+      <MenuBtn data={tool} type={type} isActive={tool[compareTarget] === props.currentItem} onClick={props.onClick} customColor={props.customColor} changeColor={props.changeColor} />
     </li>
   );
   return (
@@ -96,6 +108,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.changeMode = this.changeMode.bind(this);
+    this.changeTool = this.changeTool.bind(this);
+    this.changeItem = this.changeItem.bind(this);
+    this.changeColor = this.changeColor.bind(this);
     this.state = {
       canvasSizeX: setting.squareSize * setting.squarePerBlock * setting.mapXBlock + setting.mapPadding * 2,
       canvasSizeY: setting.squareSize * setting.squarePerBlock * setting.mapYBlock + setting.mapPadding * 2,
@@ -103,7 +118,8 @@ class App extends React.Component {
       currentMode: toolData.toolData[0].mode,
       currentModeData: toolData.toolData[0],
       currentTool: toolData.toolData[0].tools[0].tool,
-      currentItem: toolData.toolData[0].colors[0].colorName
+      currentItem: toolData.toolData[0].colors[0].colorName,
+      customColor: toolData.toolData[0].colors[toolData.toolData[0].colors.length - 1].color[0]
     };
   }
   componentDidMount() {
@@ -150,101 +166,21 @@ class App extends React.Component {
       }
     }
     this.renderGrid();
+    this.componentDidUpdate();
   }
   componentDidUpdate(prevProps) {
+    console.log(this.state.currentMode);
     if (this.state.currentMode === 'draw') {
-      paper.project.layers[2].activate();
-      const drawTool = new paper.Tool();
-      drawTool.activate();
-      
-      drawTool.onMouseDown = (e) => {
-        this.draw(e);
-      }
-      drawTool.onMouseDrag = (e) => {
-        this.draw(e);
-      }
+      this.handleDraw();
     }
-    else if (this.state.currentMode === 'build') {
-      /*paper.project.layers[3].activate();
-      const buildTool = new paper.Tool();
-      buildTool.activate();
+    else if (this.state.currentMode === 'build' && this.state.currentItem) {
+      this.handleBuildAndPlant();
       
-      let isBuild = true;
-      let isEdit = false;
-      let buildPath = new paper.Path.Rectangle({
-        x: 0, 
-        y: 0,
-        //!!!width: setting.sizeMyHome[0] * this.state.squareSize,
-        //height: setting.sizeMyHome[1] * this.state.squareSize
-      });
-      //buildPath.fillColor = '#ff84af';
-		  //buildPath.strokeColor = '#00baff';
-
-      let deletBtnBg = new paper.Path.Circle({
-        //center: [this.state.sizeMyHome[0] * this.state.squareSize, 0], 
-        radius: this.state.squareSize * 0.8
-      });
-      deletBtnBg.fillColor = '#555';
-      
-      let deletBtnIcon = new paper.PointText(new paper.Point(this.state.sizeMyHome[0] * this.state.squareSize - (this.state.squareSize * 1.6 - 6) / 2, (this.state.squareSize * 1.6 - 6) / 2));
-      deletBtnIcon.fillColor = '#eee';
-      deletBtnIcon.content = '×';
-
-      let deletBtn = new paper.Group([deletBtnBg, deletBtnIcon]);
-      deletBtn.name = 'deletBtn';
-
-      let itemSet = new paper.Group([buildPath, deletBtn]);
-
-
-
-      buildTool.onMouseMove = (e) => {
-        if (isBuild) {
-          itemSet.position = e.point;
-        }
-      }
-      buildTool.onMouseDown = (e) => {
-        if (isBuild) {
-          itemSet.position.x = e.point.x - (e.point.x - this.state.mapPadding - (this.state.sizeMyHome[0] * this.state.squareSize / 2 + this.state.squareSize * 0.4)) % this.state.squareSize;
-          itemSet.position.y = e.point.y - (e.point.y - this.state.mapPadding - (this.state.sizeMyHome[1] * this.state.squareSize / 2 + this.state.squareSize * 0.4)) % this.state.squareSize;
-          isBuild = false;
-        }
-        else {
-          const hitResult = paper.project.layers[3].hitTest(e.point, {
-            segments: true,
-            stroke: true,
-            fill: true,
-            tolerance: 5
-          });
-          if (!hitResult) {
-            isEdit = false;
-            return;
-          }
-          else {
-            if (!(hitResult.item.parent.name === 'deletBtn')) {
-              itemSet = hitResult.item.parent;
-              isEdit = true;
-            }
-            else {
-              hitResult.item.parent.parent.remove();
-            }
-          }
-        }
-      }
-      buildTool.onMouseDrag = (e) => {
-        if (isEdit) {
-          itemSet.position = e.point;
-        }
-      }
-      buildTool.onMouseUp = (e) => {
-        if (isEdit) {
-          itemSet.position.x = e.point.x - (e.point.x - setting.mapPadding - (this.state.sizeMyHome[0] * this.state.squareSize / 2 + this.state.squareSize * 0.4)) % this.state.squareSize;
-          itemSet.position.y = e.point.y - (e.point.y - setting.mapPadding - (this.state.sizeMyHome[1] * this.state.squareSize / 2 + this.state.squareSize * 0.4)) % this.state.squareSize;
-        }
-      }*/
     }
   }
   renderGrid() {
     const gridLayer = new paper.Layer();
+    gridLayer.name = 'gridLayer';
     gridLayer.activate();
     let gridLine;
     const dashStroke = [this.state.squareSize + 5, this.state.squareSize - 2]
@@ -311,6 +247,46 @@ class App extends React.Component {
       currentItem: currentItem
     });
   }
+  changeTool(e) {
+    const target = e.target.closest('button');
+    const currentTool = target.getAttribute('id');
+    let currentItem = this.state.currentItem;
+    if (this.state.currentMode !== 'draw') {
+      const toolData = this.state.currentModeData.tools.find((data) => {
+        if (data.tool === currentTool) {
+          return data;
+        }
+      });
+      currentItem = toolData.items[0].item;
+    }
+    this.setState({
+      currentTool: currentTool,
+      currentItem: currentItem
+    });
+  }
+  changeItem(e) {
+    const target = e.target.closest('button');
+    const currentItem = target.getAttribute('id');
+    this.setState({
+      currentItem: currentItem
+    });
+
+    // remove not built items
+    const checkPoint = new paper.Point(this.state.squareSize / 2 - setting.hideDist, this.state.squareSize / 2 - setting.hideDist);
+    const hitResult = paper.project.layers.buildLayer.hitTest(checkPoint, {
+      segments: true,
+      stroke: true,
+      fill: true
+    });
+    if (hitResult) {
+      hitResult.item.parent.remove();
+    }
+  }
+  changeColor(e) {
+    this.setState({
+      customColor: e.target.value
+    });
+  }
   isEditableArea(point) {
     if (point.x > setting.mapPadding && point.x < this.state.squareSize * setting.squarePerBlock * setting.mapXBlock + setting.mapPadding && point.y > setting.mapPadding && point.y < this.state.squareSize * setting.squarePerBlock * setting.mapYBlock + setting.mapPadding) {
       return true;
@@ -319,15 +295,182 @@ class App extends React.Component {
       return false;
     }
   }
+  handleDraw() {
+    paper.project.layers.drawLayer.activate();
+    const drawTool = new paper.Tool();
+    drawTool.activate();
+    
+    drawTool.onMouseDown = (e) => {
+      if (this.isEditableArea(e.point)) {
+        this.draw(e);
+      }
+    }
+    drawTool.onMouseDrag = (e) => {
+      if (this.isEditableArea(e.point)) {
+        this.draw(e);
+      }
+    }
+  }
   draw(e) {
-    if (this.isEditableArea(e.point)) {
-      const drawRect = new paper.Path.Rectangle({
-        x: e.point.x - (e.point.x - setting.mapPadding) % this.state.squareSize, 
-        y: e.point.y - (e.point.y - setting.mapPadding) % this.state.squareSize,
-        width: this.state.squareSize,
-        height: this.state.squareSize
+    paper.project.layers.drawLayer.activate();
+    // get color data 
+    let size, colors;
+    let point = new paper.Point(0, 0);
+    let testPoint = new paper.Point(0, 0);
+    const thisTool = this.state.currentModeData.tools.find((tool) => {
+      if (tool.tool === this.state.currentTool) {
+        return tool;
+      }
+    });
+    size = thisTool.size ? thisTool.size : 0;
+    if (this.state.currentItem === 'colorCustom') {
+      colors = [this.state.customColor];
+    }
+    else {
+      const thisColors = this.state.currentModeData.colors.find((color) => {
+        if (color.colorName === this.state.currentItem) {
+          return color;
+        }
       });
-      //!!!drawRect.fillColor = this.state.colorG1[Math.floor(Math.random() * this.state.colorG1.length)];
+      colors = thisColors.color;
+    }
+    const refPointDist = (size - 1) * setting.squareSize;
+
+    point.x = e.point.x - (e.point.x - setting.mapPadding) % setting.squareSize - refPointDist;
+    point.y = e.point.y - (e.point.y - setting.mapPadding) % setting.squareSize - refPointDist;
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        const drawRect = new paper.Path.Rectangle({
+          x: point.x + i * setting.squareSize, 
+          y: point.y + j * setting.squareSize,
+          width: setting.squareSize,
+          height: setting.squareSize
+        });
+        testPoint.x = point.x + i * setting.squareSize + setting.squareSize/2;
+        testPoint.y = point.y + j * setting.squareSize + setting.squareSize/2;
+        let hitResult = paper.project.layers[2].hitTest(testPoint, {            
+          fill: true
+        });
+        if (hitResult) {
+          hitResult.item.remove();
+        }
+        drawRect.fillColor = colors[Math.floor(Math.random() * colors.length)];
+      }
+    }
+  }
+  /*drawHover(e) {
+    paper.project.layers.hoverLayer.activate();
+    let size;
+    let hasHoverBlock = false;
+    console.log(paper.project.activeLayer._children.length);
+    const thisTool = this.state.currentModeData.tools.find((tool) => {
+      if (tool.tool === this.state.currentTool) {
+        return tool;
+      }
+    });
+    size = thisTool.size ? thisTool.size : 0;
+    const refPointDist = (size - 1) * setting.squareSize;
+    if (paper.project.activeLayer._children.length == 0) {
+      const drawRect = new paper.Path.Rectangle({
+        x: 0, 
+        y: 0,
+        width: setting.squareSize,
+        height: setting.squareSize
+      });
+      drawRect.strokeColor = 'rgba(255,255,255,0.8)';
+      drawRect.name = 'hoverBlock';
+    }
+    const hoverBlock = paper.project.activeLayer.children.hoverBlock;
+    hoverBlock.position.x = e.point.x - (e.point.x - setting.mapPadding) % setting.squareSize - refPointDist;
+    hoverBlock.position.y = e.point.y - (e.point.y - setting.mapPadding) % setting.squareSize - refPointDist;
+  }*/
+  handleBuildAndPlant() {
+    paper.project.layers.buildLayer.activate();
+    const buildTool = new paper.Tool();
+    buildTool.activate();
+    let size;
+    let isBuild = true;
+    let isEdit = false;
+    const thisTool = this.state.currentModeData.tools.find((tool) => {
+      if (tool.tool === this.state.currentTool) {
+        return tool;
+      }
+    });
+    const thisItem = thisTool.items.find((item) => {
+      if (item.item === this.state.currentItem) {
+        return item;
+      }
+    });
+    size = thisItem.size;
+    let buildPath = new paper.Path.Rectangle({
+      x: 0, 
+      y: 0,
+      width: size[0] * this.state.squareSize,
+      height: size[1] * this.state.squareSize
+    });
+    buildPath.fillColor = thisItem.color ? thisItem.color : thisTool.color;
+    buildPath.strokeColor = '#00baff';
+    let deletBtnBg = new paper.Path.Circle({
+      center: [size[0] * this.state.squareSize, 0], 
+      radius: this.state.squareSize * 0.8
+    });
+    deletBtnBg.fillColor = '#555';
+    let deletBtnIcon = new paper.PointText(new paper.Point(size[0] * this.state.squareSize - (this.state.squareSize * 1.6 - 6) / 2, (this.state.squareSize * 1.6 - 6) / 2));
+    deletBtnIcon.fillColor = '#eee';
+    deletBtnIcon.content = '×';
+    let deletBtn = new paper.Group([deletBtnBg, deletBtnIcon]);
+    deletBtn.name = 'deletBtn';
+    let itemSet = new paper.Group([buildPath, deletBtn]);
+    itemSet.position = [setting.hideDist * -1, setting.hideDist * -1];
+    buildTool.onMouseMove = (e) => {
+      if (isBuild) {
+        itemSet.position = e.point;
+      }
+    }
+    buildTool.onMouseDown = (e) => {
+      if (isBuild) {
+        itemSet.position.x = e.point.x - (e.point.x - setting.mapPadding - (size[0] * this.state.squareSize / 2 + this.state.squareSize * 0.4)) % this.state.squareSize;
+        itemSet.position.y = e.point.y - (e.point.y - setting.mapPadding - (size[1] * this.state.squareSize / 2 + this.state.squareSize * 0.4)) % this.state.squareSize;
+        isBuild = false;
+      }
+      else {
+        const hitResult = paper.project.layers.buildLayer.hitTest(e.point, {
+          segments: true,
+          stroke: true,
+          fill: true,
+          tolerance: 5
+        });
+        if (!hitResult) {
+          isEdit = false;
+          return;
+        }
+        else {
+          if (!(hitResult.item.parent.name === 'deletBtn')) {
+            itemSet = hitResult.item.parent;
+            isEdit = true;
+          }
+          else {
+            hitResult.item.parent.parent.remove();
+          }
+        }
+      }
+    }
+    buildTool.onMouseDrag = (e) => {
+      if (isEdit) {
+        itemSet.position = e.point;
+      }
+    }
+    buildTool.onMouseUp = (e) => {
+      if (isEdit) {
+        itemSet.position.x = e.point.x - (e.point.x - setting.mapPadding - (size[0] * this.state.squareSize / 2 + this.state.squareSize * 0.4)) % this.state.squareSize;
+        itemSet.position.y = e.point.y - (e.point.y - setting.mapPadding - (size[1] * this.state.squareSize / 2 + this.state.squareSize * 0.4)) % this.state.squareSize;
+      }
+      this.setState({
+        currentItem: null
+      });
+      document.querySelectorAll('button.item').forEach(function(btn){
+        btn.blur();
+      });
     }
   }
   render() {
@@ -339,8 +482,8 @@ class App extends React.Component {
           </div>
           
           <div className='tools'>
-            <MenuTools currentTool = {this.state.currentTool} currentModeData = {this.state.currentModeData} />
-            <MenuItems currentMode = {this.state.currentMode} currentModeData = {this.state.currentModeData} currentTool = {this.state.currentTool} currentItem = {this.state.currentItem} /> 
+            <MenuTools currentTool={this.state.currentTool} currentModeData={this.state.currentModeData} onClick={this.changeTool} />
+            <MenuItems currentMode={this.state.currentMode} currentModeData={this.state.currentModeData} currentTool={this.state.currentTool} currentItem={this.state.currentItem} onClick={this.changeItem} customColor={this.state.customColor} changeColor={this.changeColor} /> 
           </div>
         </aside>
         <canvas id='canvas'>
