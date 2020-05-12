0aa60da3c5d41ea948c22385b7ef0b25edf5b367
miau715 <miau715@gmail.com>
2020-05-13 02:21:34 +0800

    Updates

---
+++ precache-manifest.f1c6bbea431d4552108c437da261e7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171dc12e71bd6ec8358bd6ccab92c0a2",
+    "url": "/lazy-island-planner/index.html"
+  },
+  {
+    "revision": "4c6781b797acc0020510",
+    "url": "/lazy-island-planner/static/css/main.b2c1d10b.chunk.css"
+  },
+  {
+    "revision": "6919f0f16f63a956dcfb",
+    "url": "/lazy-island-planner/static/js/2.34dfd1f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lazy-island-planner/static/js/2.34dfd1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6781b797acc0020510",
+    "url": "/lazy-island-planner/static/js/main.b63d9a8f.chunk.js"
+  },
+  {
+    "revision": "41e25e3de727faf5e251",
+    "url": "/lazy-island-planner/static/js/runtime-main.f1b090b0.js"
+  }
+]);
